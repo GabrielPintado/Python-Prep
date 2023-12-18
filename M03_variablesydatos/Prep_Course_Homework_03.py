63807e3c01fc8a3a6deb78b1bd0936825a1b1d99
GabrielPintado <147005294+GabrielPintado@users.noreply.github.com>
2023-12-18 15:53:27 -0500

    Update Prep_Course_Homework_03.py

--- M03_variablesydatos/Prep_Course_Homework_03.py
+++ M03_variablesydatos/Prep_Course_Homework_03.py
@@ -6,7 +6,8 @@
 # 1) Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla
 
 # In[7]:
-
+number = 7
+print(number)
 
 
 
@@ -14,7 +15,7 @@
 
 # In[3]:
 
-
+print(type(8.5))
 
 
 
@@ -22,14 +23,14 @@
 
 # In[8]:
 
-
+print(type(number))
 
 
 
 # 4) Crear una variable que contenga tu nombre
 
 # In[2]:
-
+name = "Gabriel"
 
 
 
@@ -37,7 +38,7 @@
 
 # In[3]:
 
-
+complex = 9 + -4h
 
 
 
@@ -45,6 +46,7 @@
 
 # In[4]:
 
+print(type(complex))
 
 
 
@@ -56,12 +58,15 @@
 
 pi = 3.1416
 
+round4 = round(pi, 4)
+
 
 # 8) Crear una variable que contenga el valor 'True' y otra que contenga el valor True. ¿Se trata de lo mismo?
 
 # In[3]:
 
-
+va1 = "True"
+va2 = True
 
 
 
@@ -69,6 +74,8 @@ pi = 3.1416
 
 # In[5]:
 
+print(type(va1))
+print(type(va2))
 
 
 
@@ -76,7 +83,7 @@ pi = 3.1416
 # 10) Asignar a una variable, la suma de un número entero y otro decimal
 
 # In[1]:
-
+va3 = 4 + 8.2
 
 
 
@@ -85,7 +92,9 @@ pi = 3.1416
 
 # In[2]:
 
-
+va = 4 - 8g
+va4 = 5 + 4g
+print(va + va5)
 
 
 
@@ -93,7 +102,7 @@ pi = 3.1416
 
 # In[4]:
 
-
+print(2h + 9)
 
 
 
@@ -101,7 +110,7 @@ pi = 3.1416
 
 # In[5]:
 
-
+print(8 * 9)
 
 
 
@@ -109,14 +118,14 @@ pi = 3.1416
 
 # In[6]:
 
-
+print(2**8
 
 
 # 15) Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla
 
 # In[8]:
-
-
+var = 27/4
+print(var)
 
 
 
@@ -124,7 +133,7 @@ pi = 3.1416
 
 # In[9]:
 
-
+print(27//4)
 
 
 
@@ -132,14 +141,14 @@ pi = 3.1416
 
 # In[1]:
 
-
+print(27 % 4)
 
 
 
 # 18) Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 16 y 17. Obtener 27 como resultado
 
 # In[2]:
-
+print(4 * (27//4) + (27 % 4))
 
 
 
@@ -148,15 +157,16 @@ pi = 3.1416
 
 # In[3]:
 
+vari = "Hola"
+vari2 = "Mundo"
 
-
-
+vari3 = vari1 + vari2
 
 # 20) Evaluar si "2" es igual a 2. ¿Por qué ocurre eso?
 
 # In[4]:
 
-
+vari3 = "2" == 2
 
 
 
@@ -164,13 +174,15 @@ pi = 3.1416
 
 # In[11]:
 
-
+vari4 = "2" == str(2)
 
 
 
 # 22) ¿Por qué arroja error el siguiente cambio de tipo de datos? a = float('3,8')
 
 # In[12]:
+por la coma
+a = float("3,8")
 
 
 
@@ -179,7 +191,8 @@ pi = 3.1416
 # 23) Crear una variable con el valor 3, y utilizar el operador '-=' para modificar su contenido y que de como resultado 2.
 
 # In[15]:
-
+varia = 3
+varia -= 1
 
 
 
@@ -189,6 +202,8 @@ pi = 3.1416
 # In[29]:
 
 
+"""Porque el 1 o 001 esta en la primera casilla, el "<<" mueve al 1 o 001 2 casillas resultando en la tercera casilla y por lo tanto en un 100 o 4 en binario"""
+
 
 
 
@@ -196,6 +211,7 @@ pi = 3.1416
 
 # In[23]:
 
+""" Porque son de distinto tipo (entero y string respectivamente) por eso no pueden sumarse, en caso de que ambos fueran enteros el resultado daria 4, en cambio si ambos fueran string el resultado seria "22"."""
 
 
 
@@ -205,5 +221,6 @@ pi = 3.1416
 
 # In[30]:
 
-
+print(2 + 2)
+print("2" + "2")
 
